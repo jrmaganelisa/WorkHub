f948bee03c4109b9cab6839935d5ae0541ed7612
jrmaganelisa <judith.maganelisa@absa.co.za>
2018-05-10 15:00:43 +0200

    small

---
+++ AngularjsMvc/Models/EF/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AngularjsMvc.Models.EF
+{
+    public class User
+    {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Surname { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string IdentityType { get; set; }
+
+        [StringLength(50, MinimumLength = 13)]
+        public string IdentityNumber { get; set; }
+
+        [StringLength(50)]
+        public string DateOfBirth { get; set; }
+    }
+}
---
+++ AngularjsMvc/app/controllers/userCtrl.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('userCtrl', ['$scope', 'dataService', function ($scope, dataService) {
+            $scope.users = [];
+           
+            getData();
+
+            //display user data
+            function getData() {
+                dataService.getUsers().then(function (result) {
+                   
+                    $scope.users = result;
+                });
+            }
+
+            //delete user based on id
+            $scope.deleteUser = function (id) {
+                dataService.deleteUser(id).then(function () {
+                    toastr.success('User deleted successfully');
+                    getData();
+                }, function () {
+                    toastr.error('Error in deleting user with Id: ' + id);
+                });
+            };
+
+            
+        }])
+
+        //add user controller
+        .controller('userAddCtrl', ['$scope', '$location', 'dataService', function ($scope, $location, dataService) {
+            $scope.createUser = function (user) {
+                dataService.addUser(user).then(function () {
+                    toastr.success('User created successfully');
+                    $location.path('/');
+                }, function () {
+                    toastr.error('Error in creating user');
+                });
+            };
+        }])
+
+        //edit user controller
+        .controller('userEditCtrl', ['$scope', '$routeParams', '$location', 'dataService', function ($scope, $routeParams, $location, dataService) {
+            $scope.user = {};
+            $scope.states = {
+                showUpdateButton: false
+            };
+
+            dataService.getUserById($routeParams.id).then(function (result) {
+                $scope.user = result;
+                $scope.states.showUpdateButton = true;
+            }, function () {
+                toastr.error('Error in fetching user with Id: ' + $routeParams.id);
+            });
+
+            $scope.updateUser = function (user) {
+                dataService.editUser(user).then(function () {
+                    toastr.success('User updated successfully');
+                    $location.path('/');
+                }, function () {
+                    toastr.error('Error in updating user');
+                });
+            };
+        }]);
+})();
